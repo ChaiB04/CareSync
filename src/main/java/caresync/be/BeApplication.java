a584ffee5d8afd50d6869c38c448b48134a8f4bc
ChaimaiBahari <chaimai.bahari@quadsolutions.nl>
2025-03-13 10:10:51 +0100

    empty commit

--- src/main/java/caresync/be/BeApplication.java
+++ src/main/java/caresync/be/BeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeApplication {
-//    empty commit
     public static void main(String[] args) {
         SpringApplication.run(BeApplication.class, args);
     }
