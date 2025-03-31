db16ff0d33f54ee6516923595ecb512253b14df1
ChaimaiBahari <chaimai.bahari@quadsolutions.nl>
2025-03-31 14:39:02 +0200

    removed logging

--- src/main/java/caresync/be/controller/UserController.java
+++ src/main/java/caresync/be/controller/UserController.java
@@ -1,8 +1,6 @@
 package caresync.be.controller;
 
 import lombok.AllArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,18 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class UserController {
 
-    private final Logger logger = LogManager.getLogger(UserController.class);
 
     @GetMapping()
     public String getUsers() {
        try{
-           logger.info("getUsers");
            return "users";
        }
        catch (Exception e){
-           logger.error(e);
+
        }
-       logger.warn("outside of try-catch");
        return "error";
     }
 }
